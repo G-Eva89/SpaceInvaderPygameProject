29c0d9d997278e3f95fc1903bd621afcad2bbe28
G-Eva89 <gevagupta@gmail.com>
2021-12-19 22:44:08 +0530

    Background

--- main.py
+++ main.py
@@ -9,19 +9,8 @@ screen = pygame.display.set_mode((1200,600))
 
 #adding a title and an icon to the screen
 pygame.display.set_caption("Space Invaders")
-gameicon = pygame.image.load("spaceship2.png")
+gameicon = pygame.image.load("spacetravel.png")
 pygame.display.set_icon(gameicon)
-
-#adding player to the game
-playericon = pygame.image.load("spaceship.png")
-
-#initial coordinates to the position of player
-playerX = 568   #1200/2 - 64/2; 64 is the pixel size of playericon
-playerY = 440   
-
-def player(x,y):
-    #draws the image of player at the given coordinates
-    screen.blit(playericon, (x,y))
 
 running = True
 while running:
@@ -35,9 +24,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
           
-    
 
-    player(playerX,playerY)
-    
     #update the screen to reflect the changes made in this while loop
     pygame.display.update()
